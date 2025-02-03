dbd58134315c7c63b0121f29a6886b0b23586aa5
nade <nagano.kazuma.na5@gmail.com>
2025-02-03 18:21:30 +0900

    Update xcode.sh

--- scripts/xcode.sh
+++ scripts/xcode.sh
@@ -2,24 +2,30 @@
 
 echo "Xcode のセットアップ..."
 
-if ! xcodes list &> /dev/null; then
-  echo "⚠️ Xcodes にログインする必要があります。Xcodes.app を開いて Apple ID でログインしてください。"
-  open -a "Xcodes"
+# Xcodesが使えない環境ではスキップ
+# if ! xcodes list &> /dev/null; then
+#   echo "⚠️ Xcodes にログインする必要があります。Xcodes.app を開いて Apple ID でログインしてください。"
+#   open -a "Xcodes"
   
-  osascript -e 'tell app "System Events" to display dialog "Xcodes.app で Apple ID にログインしてください。\nログイン完了後に OK を押してください。" buttons {"OK"} default button "OK"'
+#   osascript -e 'tell app "System Events" to display dialog "Xcodes.app で Apple ID にログインしてください。\nログイン完了後に OK を押してください。" buttons {"OK"} default button "OK"'
 
-  until xcodes list &> /dev/null; do
-    echo "Xcodes にログインが完了していません。ログインを確認中..."
-    sleep 5
-  done
-fi
+#   until xcodes list &> /dev/null; do
+#     echo "Xcodes にログインが完了していません。ログインを確認中..."
+#     sleep 5
+#   done
+# fi
 
-xcodes update
-LATEST_XCODE=$(xcodes list --latest)
-xcodes install "$LATEST_XCODE" --experimental-unxip --latest
-xcodes select "$LATEST_XCODE"
+# xcodes update
+# LATEST_XCODE=$(xcodes list --latest)
+# xcodes install "$LATEST_XCODE" --experimental-unxip --latest
+# xcodes select "$LATEST_XCODE"
 
-sudo xcodebuild -license accept
-sudo xcode-select --switch /Applications/Xcode.app/Contents/Developer
+# sudo xcodebuild -license accept
+# sudo xcode-select --switch /Applications/Xcode.app/Contents/Developer
+
+# Xcode のフィンガープリント検証をスキップ
+echo "Xcode のパッケージプラグイン & マクロのフィンガープリント検証をスキップ..."
+defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidatation -bool YES
+defaults write com.apple.dt.Xcode IDESkipMacroFingerprintValidation -bool YES
 
 echo "Xcode のセットアップが完了しました！"
